8842e7c7cd927e1c764115f61da0e3b4b06f178a
Moksha Suthar <mokshasuthar252@gmail.com>
2026-03-12 15:03:34 +0530

    done

--- ca_cal/settings.py
+++ ca_cal/settings.py
@@ -33,7 +33,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mokshasuthar252@gmail.com'           # Your Gmail
-EMAIL_HOST_PASSWORD = 'pcwvzodifzwqsqx'          # Gmail App Password
+EMAIL_HOST_PASSWORD = 'vrboxtzzsififuoe'          # Gmail App Password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
