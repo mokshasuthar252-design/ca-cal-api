243c7d8dae540f2729846dac16a94c781604ee43
Moksha Suthar <mokshasuthar252@gmail.com>
2026-03-12 13:40:33 +0530

    all done

--- ca_app/mongo.py
+++ ca_app/mongo.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://moksha:moksha252@cluster0.ahzyhcp.mongodb.net/?appName=Cluster0")
+
 db = client["calculator_db"]
 
 user_collection = db["users"]
 history_collection = db["calculator_history"]
+
